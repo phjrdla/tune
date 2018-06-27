76b9af583f87d4688ab3ec90db1394628959e646
phjrdla <30568443+phjrdla@users.noreply.github.com>
2018-06-27 10:36:12 +0200

    ttbox

--- cr_tuning_task_w_sql_id_v2.sql
+++ cr_tuning_task_w_sql_id_v2.sql
@@ -1,19 +1,19 @@
--- SQL Tuning Advisor
-variable sts_task datatype varchar2(30)
 
+-- SQL Tuning Advisor
+variable sts_task datatype varchar2(30);
+
+set serveroutput on
 declare
 sts_task VARCHAR2(64);
-
 BEGIN
   -- Create a tuning task, sort on elapsed time
-  sts_task := DBMS_SQLTUNE.CREATE_TUNING_TASK( sqlset_name => 'STS02' 
-                                              ,rank1 => 'ELAPSED_TIME'
+  sts_task := DBMS_SQLTUNE.CREATE_TUNING_TASK( sql_id => '6j294p5u2vhbk' 
                                               ,time_limit => 7200
 											  ,scope => DBMS_SQLTUNE.scope_comprehensive
-                                              ,description => 'tune my workload ordered by elapsed time');
+                                              ,description => 'tune query on orlsol00');
                                               
   DBMS_SQLTUNE.EXECUTE_TUNING_TASK( task_name => sts_task );
-  DBMS_output.put_line('sts_task is '||to_char(sts_task) );
+  dbms_Output.put_line(sts_task);
 end;
 /
 -- Task name
@@ -21,6 +21,7 @@ print sts_task
 
 select :sts_task from dual;
 
+exit
 SELECT TASK_NAME
 FROM DBA_ADVISOR_LOG
 where task_name like 'TASK%' order by 1;
@@ -29,17 +30,14 @@ rem Résultats de la tache de Tuning avec recommendations
 SET LONG 10000000
 SET LONGCHUNKSIZE 1000
 SET LINESIZE 100
-SELECT DBMS_SQLTUNE.REPORT_TUNING_TASK('&taskname'  , 'TEXT', 'TYPICAL', 'ALL')
+SELECT DBMS_SQLTUNE.REPORT_TUNING_TASK(upper('&taskname')  , 'TEXT', 'TYPICAL', 'ALL')
 FROM DUAL;
 
 rem Scripts pour implémenter les recommandations
-SELECT dbms_sqltune.script_tuning_task('TASK_13100', 'ALL') FROM dual;
-
-
+SELECT dbms_sqltune.script_tuning_task(upper('&taskname'), 'ALL') FROM dual;
 
 SELECT TASK_NAME
-FROM DBA_ADVISOR_LOG
-order by task_name;
+FROM user_ADVISOR_LOG
 where task_name like 'TASK%' order by 1;
 
-exec DBMS_SQLTUNE.DROP_TUNING_TASK('TASK_13098');
+exec DBMS_SQLTUNE.DROP_TUNING_TASK(upper('&task'));
