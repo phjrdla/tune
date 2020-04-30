987c45a98bd0bdbb714d4aab5164a5fcc9558ede
phjrdla <30568443+phjrdla@users.noreply.github.com>
2020-04-30 11:47:37 +0200

    ts temp usage

---
+++ q_temp_usage.sql
@@ -0,0 +1,31 @@
+/* Formatted on 3/20/2020 12:22:49 PM (QP5 v5.336) */
+set lines 200
+col username format a20 trunc
+col osuser format a20 trunc
+  SELECT S.sid || ',
+         ' || S.serial# sid_serial,
+         S.username,
+         S.osuser,
+         P.spid,
+         S.module,
+         S.program,
+         SUM (T.blocks) * TBS.block_size / 1024 / 1024     mb_used,
+         T.tablespace,
+         COUNT (*)  sort_ops
+    FROM v$sort_usage   T,
+         v$session      S,
+         dba_tablespaces TBS,
+         v$process      P
+   WHERE     T.session_addr = S.saddr
+         AND S.paddr = P.addr
+         AND T.tablespace = TBS.tablespace_name
+GROUP BY S.sid,
+         S.serial#,
+         S.username,
+         S.osuser,
+         P.spid,
+         S.module,
+         S.program,
+         TBS.block_size,
+         T.tablespace
+ORDER BY sid_serial;
---
+++ q_temp_usage_user.sql
@@ -0,0 +1,33 @@
+--
+-- Displays the temp sort space currently in use by users.
+--
+ 
+-- SET PAUSE ON
+-- SET PAUSE 'Press Return to Continue'
+set pause off
+SET PAGESIZE 50
+SET LINESIZE 300
+ 
+COLUMN tablespace FORMAT A20
+COLUMN temp_size FORMAT A20
+COLUMN sid_serial FORMAT A20
+COLUMN username FORMAT A28
+COLUMN program FORMAT A50
+ 
+SELECT b.tablespace,
+       ROUND(((b.blocks*p.value)/1024/1024),2)||'M' AS temp_size,
+       a.inst_id as Instance,
+       a.sid||','||a.serial# AS sid_serial,
+       NVL(a.username, '(oracle)') AS username,
+       a.program,
+       a.status,
+       a.sql_id
+FROM   gv$session a,
+       gv$sort_usage b,
+       gv$parameter p
+WHERE  p.name  = 'db_block_size'
+AND    a.saddr = b.session_addr
+AND    a.inst_id=b.inst_id
+AND    a.inst_id=p.inst_id
+ORDER BY b.tablespace, b.blocks
+/
